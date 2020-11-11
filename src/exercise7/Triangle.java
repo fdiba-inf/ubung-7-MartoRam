8a1cbcc183fef75731fda4efc31553891548168e
MartoRam <mmmladenov1@gmail.com>
2020-11-11 23:50:06 +0200

    proba 2-ne vijdame greshkka/exper

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -6,7 +6,7 @@ public class Triangle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter n: ");
+        System.out.println("Enter n: ");
         int n = input.nextInt();
 
 
