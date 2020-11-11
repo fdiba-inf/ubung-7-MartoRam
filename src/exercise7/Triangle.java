b45c1be5c7cf3f17d74e055226522a89fac31eae
MartoRam <mmmladenov1@gmail.com>
2020-11-11 23:40:22 +0200

    proba 1

--- src/exercise7/Triangle.java
+++ src/exercise7/Triangle.java
@@ -9,14 +9,31 @@ public class Triangle {
         System.out.print("Enter n: ");
         int n = input.nextInt();
 
-        for (int line = 1; line <= n; line++) {
-            printLine(1, line);
-        }
+
+            printLine(1, n);
+
 
         // Print the bottom part of the triangle that is under the longest line
     }
 
     public static void printLine(int start, int end) {
         // Implement method
+
+            String bull="";
+            for (int j=start;j<=end;j++)
+            {
+                bull=bull+j+" ";
+                System.out.println(bull);
+            }
+            for (int k=start;k<end;k++)
+            {
+                int i=2;
+                bull=bull.substring(0, bull.length()-i);
+                System.out.println(bull);
+                i++;
+            }
+
+
+
     }
 }
